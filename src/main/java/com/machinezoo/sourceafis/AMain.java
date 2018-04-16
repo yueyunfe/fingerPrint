a33156b42947ef7fe4b80f4326c53d5c62a71920
yueyunfe <yueyunfe@husky.neu.edu>
2018-04-16 05:29:51 -0700

    update images

--- src/main/java/com/machinezoo/sourceafis/AMain.java
+++ src/main/java/com/machinezoo/sourceafis/AMain.java
@@ -5,36 +5,65 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AMain {
+
+    private static List<candidateMatching> candidateList = new ArrayList<>();
+    private static List<Double> scores = new ArrayList<>();
+    private static String[] names =new String[]{"backgroundfilter","contrast","halfblur","linefilter","pixelate3","rotate90","shadow","transparency","penarteffect","thermalFilter","freedraw"};
     public static void main(String[] argv){
         try {
-            FilePath filePath1 = new FilePath("probe.png");
-            FilePath filePath2 = new FilePath("nonmatching.png");
+            initialCandidate();
 
+            FilePath filePath1 = new FilePath("probe.png");
             byte[] probeImage = Files.readAllBytes(filePath1.getFilePath());
-            byte[] candidateImage = Files.readAllBytes(filePath2.getFilePath());
+
             FingerprintTemplate probe = new FingerprintTemplate()
                     .dpi(500)
                     .create(probeImage);
-            FingerprintTemplate candidate = new FingerprintTemplate()
-                    .dpi(500)
-                    .create(candidateImage);
 
-            double score = new FingerprintMatcher()
-                    .index(probe)
-                    .match(candidate);
 
-            if(score > 50){
-                System.out.println("Matching");
-            }else{
-                System.out.println("NonMatching");
+            for(int i = 0; i < candidateList.size(); i++){
+                FilePath candidatePath = new FilePath(candidateList.get(i).getName()+".png");
+                byte[] candidateImage = Files.readAllBytes(candidatePath.getFilePath());
+                FingerprintTemplate candidate = new FingerprintTemplate()
+                        .dpi(500)
+                        .create(candidateImage);
+                double score = new FingerprintMatcher()
+                        .index(probe)
+                        .match(candidate);
+                scores.add(score);
             }
+
+            for(int i = 0; i < scores.size(); i++){
+                String words = "\nName: " + names[i] + "     Matching Score: "+ scores.get(i);
+                System.out.println(words);
+            }
+
+
+
+
+
+
+
+//            if(score > 50){
+//                System.out.println("Matching");
+//            }else{
+//                System.out.println("NonMatching");
+//            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void initialCandidate(){
+        for(int i = 0; i < names.length; i++){
+            candidateMatching candidateMatching = new candidateMatching(names[i]);
+            candidateList.add(candidateMatching);
+        }
 
     }
 }
---
+++ src/main/java/com/machinezoo/sourceafis/candidateMatching.java
@@ -0,0 +1,14 @@
+package com.machinezoo.sourceafis;
+
+public class candidateMatching {
+
+    String name;
+
+    public candidateMatching(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
